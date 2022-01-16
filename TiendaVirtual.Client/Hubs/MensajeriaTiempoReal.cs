f0b56a0f3349fec0b5eb713786c09dce3cd30bcb
Edison Peña <thu.acotisao2018@gmail.com>
2022-01-16 17:06:18 -0400

    -Se agrego paquetes de signal R core
    -Creacion de clase para comunicacion signal R

---
+++ TiendaVirtual.Client/Hubs/MensajeriaTiempoReal.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TiendaVirtual.Client.Hubs
+{
+    public class MensajeriaTiempoReal:Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public Task MensajeGenerar(string mensaje)
+        {
+            return Clients.Client("").SendAsync("MensajeriaGeneral", mensaje);
+        }
+    }
+}
